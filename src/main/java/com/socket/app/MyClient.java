387c2635b7299a75d6db7c1c65e747c619bb029d
Yildiray Elmaci <yildiray.elmaci@asml.com>
2023-10-25 08:37:12 +0200

    second commit

---
+++ src/main/java/com/socket/app/MyClient.java
@@ -0,0 +1,97 @@
+package com.socket.app;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.SyncFailedException;
+import java.net.Socket;
+
+public class MyClient {
+
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port) {
+        try {
+            clientSocket = new Socket(ip, port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(String.format("Connection is not established : error => %s", e.getMessage()));
+        }
+
+
+    }
+
+    public String sendMessage(String msg) {
+        try {
+            System.out.println(String.format("sending message to the server : %s", msg));
+            out.println(msg);
+            String resp = in.readLine();
+            return resp;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(String.format("Unexpeced error : %s", e.getMessage()));
+            return null;
+        }
+    }
+
+    public void stopConnection() {
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+    private static final int MODE_READ = 0;
+    private static final int MODE_WRITE = 1;
+
+    public static void main(String[] args) {
+
+        int mode = MODE_READ;
+
+        String msgToSend  = null;
+        if (args.length==1) {
+            String cllientId = args[0];
+            msgToSend = String.format("read %s",cllientId);
+            mode = MODE_READ;
+        }
+        else if (args.length==3) {
+            String cllientId = args[0];
+            String firstName = args[1];
+            String lastName = args[2];
+            msgToSend =String.format("write %s|%s|%s", cllientId, firstName, lastName);
+            mode = MODE_WRITE;
+        }
+        else {
+            System.out.println("invalid arguments");
+            System.exit(0);
+        }
+
+        MyClient client = new MyClient();
+        client.startConnection("127.0.0.1", 7777);
+        String response = client.sendMessage(msgToSend);
+        System.out.println("from server : " + response);
+        if (mode == MODE_READ) {
+            if (response.equals("<NO_DATA_FOUND>")) {
+                System.out.println("No data found in database");
+                System.exit(0);
+            }
+
+            String[] arr = response.split("\\|");
+            String firstName = arr[0];
+            String lasName = arr[1];
+            System.out.println(String.format("This is your data read from server database: First Name :  %s, Last Name : %s", firstName, lasName));
+        } else {
+            System.out.println(String.format("Your data is successfully written to server database"));
+        }
+        client.stopConnection();
+    }
+
+
+}
---
+++ src/main/java/com/socket/app/MyServer.java
@@ -0,0 +1,91 @@
+package com.socket.app;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class MyServer {
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    HashMap<String, String> hmData = new HashMap<>();
+
+    public void startServer(int port) {
+        try {
+            System.out.println(String.format("Server socket listening port %d", port));
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            while(true) {
+                String command = in.readLine();
+
+                if (command == null) {
+                    try { Thread.sleep(100); } catch (Exception e) {}
+                    continue;
+                }
+                System.out.println(String.format("new command received from a cliend : %s" , command));
+                if (command.startsWith("write ")) {
+                    String data = command.substring(6).strip();
+                    String[] arr = data.split("\\|");
+                    String clientId = arr[0];
+                    String firstName = arr[1];
+                    String lastName = arr[2];
+
+                    System.out.println(String.format("writing to database : [%s] = %s, %s", clientId, firstName, lastName));
+                    hmData.put(clientId, String.format("%s|%s", firstName, lastName));
+                    out.println("<SUCCESS>");
+                    out.flush();
+
+                }
+                else if (command.startsWith("read ")) {
+                    String clientId = command.substring(5).strip();
+                    if (hmData.containsKey(clientId)) {
+                        System.out.println("data found for client : " + clientId);
+                        out.println(hmData.get(clientId));
+                    } else {
+                        System.out.println("data not found for client : " + clientId);
+                        out.println("<NO_DATA_FOUND>");
+                    }
+                }
+                else {
+                    out.println("unrecognised command");
+                }
+
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(String.format("Unexpected error on server : %s", e.getMessage()));
+        }
+
+    }
+
+    public  void stop() {
+        try {
+            System.out.println("stopping server...");
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+
+    public static void main(String[] args)  {
+        int port = 7777;
+        MyServer server = new MyServer();
+        server.startServer(port);
+        server.stop();
+    }
+}
